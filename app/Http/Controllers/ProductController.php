a786fc381fbe53d308019bdab366f95ec5061d11
christian <christianglenogo@gmail.com>
2024-04-20 22:29:28 +0800

    product api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use Exception;
 
 class ProductController extends Controller
 {
@@ -11,6 +15,19 @@ class ProductController extends Controller
      */
     public function index()
     {
+        try {
+            $product = Product::paginate(10);
+
+            return response()->json([
+                "success" => true,
+                "data" => $product
+            ], 200);
+        } catch (\Exception $e) {
+            $errorMessage = $e->getMessage();
+            return response()->json([
+                "error" => $errorMessage
+            ], 500);
+        }
     }
 
     /**
@@ -24,17 +41,42 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
-        //
+        try {
+            $productData = $request->validated();
+            $product = Product::create($productData);
+
+            return  response()->json(['data' => $product], 200);
+        } catch (\Exception $e) {
+            $errorMessage = $e->getMessage();
+            return response()->json(['error' => $errorMessage], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($product)
     {
-        //
+        try {
+            $productData = Product::find($product);
+
+            if (empty($productData)) {
+                return response()->json([
+                    'success' => false,
+                    'data' => 'not found!'
+                ], 404);
+            }
+
+            return response()->json([
+                'success' => true,
+                'data' => $productData
+            ], 200);
+        } catch (\Exception $e) {
+            $errorMessage = $e->getMessage();
+            return response()->json(['message' => $errorMessage], 500);
+        }
     }
 
     /**
@@ -48,16 +90,58 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateProductRequest $request, $product)
     {
-        //
+        try {
+            $productData = Product::find($product);
+
+            if (empty($productData)) {
+                return response()->json([
+                    'success' => false,
+                    'data' => "not found!"
+                ], 404);
+            }
+
+            $updateData = $request->validated();
+            Product::updataOrCreate($updateData);
+
+            return response()->json([
+                "success" => true,
+                "message" => "Product updated"
+            ], 200);
+        } catch (\Exception $e) {
+            $errorMessage = $e->getMessage();
+            return response()->json([
+                "error" => $errorMessage
+            ], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($product)
     {
-        //
+        try {
+            $productData = Product::find($product);
+
+            if (empty($productData)) {
+                return response()->json([
+                    "success" => false,
+                    "data" => "not found"
+                ], 404);
+            }
+
+            $productData->delete();
+            return response()->json([
+                "success" => false,
+                "data" => "Product deleted"
+            ], 200);
+        } catch (\Exception $e) {
+            $errorMessage = $e->getMessage();
+            return response()->json([
+                'error' => $errorMessage
+            ], 500);
+        }
     }
 }
